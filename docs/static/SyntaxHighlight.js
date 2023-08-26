3a74bb669c5a484f68ed696ff3003d21f59e9ac2
Elijah <46640763+a-riceeater@users.noreply.github.com>
2023-08-26 01:14:32 -0700

    Update SyntaxHighlight.js

--- docs/static/SyntaxHighlight.js
+++ docs/static/SyntaxHighlight.js
@@ -118,6 +118,25 @@ class SyntaxHighlight {
     }
 }
 
+function insertAtCursor(myField, myValue) {
+    //IE support
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+    }
+    //MOZILLA and others
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        myField.value = myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+    } else {
+        myField.value += myValue;
+    }
+}
+
 // Test
 /*
 const element = new SyntaxHighlight(document.getElementById("code"));
